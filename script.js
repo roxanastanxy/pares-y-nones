9e981a2adab8e6f07e6822a83206ec9ea1edabfe
roxanastanxy <roxana.stan.valdepenas@gmail.com>
2017-09-28 14:57:11 +0200

    a

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('no').onclick=function(){
+    document.getElementById('positivo').style.display='none';
+    document.getElementById('negativo').style.display='block';
+}
+document.getElementById('si').onclick=function(){
+   document.getElementById('positivo').style.display='none';
+    document.getElementById('negativo').style.display='block';
+}
